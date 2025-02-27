486c4053c51fa6d2bf9ae9bf77cc12989000beee
Ricardo Felipe Benjamin <ricardofelipebenjamin2003@gmail.com>
2025-02-27 09:29:35 -0300

    Segundo commit

--- Alunos/Ricardo Felipe Benjamin/aula02-git/Hello.java
+++ Alunos/Ricardo Felipe Benjamin/aula02-git/Hello.java
@@ -6,6 +6,10 @@ public class Hello {
     public static void HelloWorld(){
         System.out.println("Hello World");
     }
+
+    public static void OlaMundo(){
+        System.out.println("Ola Mundo");
+    }
     public static void main(String[] args) {
 
     Scanner sc =  new Scanner(System.in);
@@ -13,10 +17,13 @@ public class Hello {
 
         System.out.println("Escolha uma Opção: ");
         System.out.println("[1]Imprimir Hello World");
+        System.out.println("[2]Imprimir Ola Mundo ");
         option = sc.nextInt();
 
-            if(option == 1){
+            if (option == 1){
                 HelloWorld();
+            }else if (option == 2){
+                OlaMundo();
             }
         
     }
