a1484ba144b7cf1019d26914b9f0682cb039630b
Ricardo Felipe Benjamin <ricardofelipebenjamin2003@gmail.com>
2025-02-27 09:40:32 -0300

    terceiro commit final

--- Alunos/Ricardo Felipe Benjamin/aula02-git/Hello.java
+++ Alunos/Ricardo Felipe Benjamin/aula02-git/Hello.java
@@ -10,21 +10,36 @@ public class Hello {
     public static void OlaMundo(){
         System.out.println("Ola Mundo");
     }
+
+    public static void OlaQueTal(){
+        System.out.println("Ola Que Tal");
+    }
+
     public static void main(String[] args) {
 
-    Scanner sc =  new Scanner(System.in);
-    int option;
+      Scanner sc =  new Scanner(System.in);
+      int option = 0;
 
-        System.out.println("Escolha uma Opção: ");
-        System.out.println("[1]Imprimir Hello World");
-        System.out.println("[2]Imprimir Ola Mundo ");
-        option = sc.nextInt();
+        while(option != 4){ 
+            System.out.println("Escolha uma Opção: ");
+            System.out.println("[1]Imprimir Hello World");
+            System.out.println("[2]Imprimir Ola Mundo ");
+            System.out.println("[3]Imprimir Ola Que Tal");
+            System.out.println("[4]Sair");
+            option = sc.nextInt();
 
-            if (option == 1){
+            if(option == 1){
                 HelloWorld();
-            }else if (option == 2){
+            }else if(option == 2){
                 OlaMundo();
+            }else if(option == 3){
+                OlaQueTal();
+            }else if(option == 4){
+                System.out.println("Saindo...");
+            }else{
+                System.out.println("Error");
             }
+        }
         
     }
 
