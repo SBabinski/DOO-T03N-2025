225df34fafa82c5ce218e64473ed2e9b9ba11c6e
EduardoFontanela <ejrfontanela@minha.fag.edu.br>
2025-03-07 19:31:18 -0300

    Aula01-02

---
+++ Alunos/Eduardo Fontanela/Aula01/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    public static void olaMundo() {
+        System.out.println("Olá Mundo");
+    }
+
+    public static void holaQueTal() {
+        System.out.println("Hola que tal");
+    }
+}
