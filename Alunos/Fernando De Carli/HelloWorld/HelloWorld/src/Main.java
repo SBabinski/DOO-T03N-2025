32e190b899e42bb56630bf4b2f048ff02280b780
fernando-de-carli <fernandodecarli05@gmail.com>
2025-03-10 21:15:00 -0300

    Espanhou

--- Alunos/Fernando De Carli/HelloWorld/HelloWorld/src/Main.java
+++ Alunos/Fernando De Carli/HelloWorld/HelloWorld/src/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void helloWorld() {
         System.out.println("Hello World!");
     }
-
+    public static void holaquetal() {
+        System.out.println("Hola que tal");
+    }
 }
