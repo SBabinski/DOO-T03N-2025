e2ffb78f78bfbe0c159d2bbe178992cf7bd79daf
AndreyRP <119114347+AndreyRP@users.noreply.github.com>
2025-04-10 21:07:15 -0300

    Delete Alunos directory

--- Alunos/Braian/Aula04/venda.java
+++
@@ -1,8 +0,0 @@
-package Alunos.Braian.Aula04;
-
-public class venda extends transacao {
-
-    public venda(int quantidade, double precoUnitario, String data) {
-        super(quantidade, precoUnitario, data);
-    }
-}
--- Alunos/Kendra/Aula01/Hello.java
+++
@@ -1,8 +0,0 @@
-package Alunos.Kendra.Aula01;
-
-public class Hello{
-    public static void main(String[]args){
-       System.out.println("Hello World!");
-
-    }
-}
